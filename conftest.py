ac97692fb95f77e139e1855196e1ced8a6e4f19c
Anna <a.nedorya@yandex.ru>
2019-04-19 19:09:02 -0400

    Попытка сделать генерацию тестовых данных из excel

--- conftest.py
+++ conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from fixture.application import Application
+import pandas
 
 @pytest.fixture(scope="session")
 def app(request):
     fixture = Application("D:\\Anna\AddressBook\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+# def pytest_generate_tests(metafunc):
+#     for fixture in metafunc.fixturenames:
+#         data_from_excel = pandas.read_excel("D:\\Anna\python-gui-tests\\groups.xlsx")
+#         metafunc.parametrize(fixture, data_from_excel, ids=[str(x) for x in data_from_excel])
+#
+#
